5fa1284b30f6969feaeafd77ed6f9e44f6dfecea
duclux <duclux@gmail.com>
2020-09-14 01:03:03 +0700

    add somm

---
+++ server/src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    logger: ['warn', 'error'],
+  });
+
+  app.setGlobalPrefix('/api');
+  const PORT = process.env.PORT || 5000;
+  await app.listen(PORT, () => {
+    console.log('Server is running on port', PORT);
+  });
+}
+bootstrap();
