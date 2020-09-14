705cf6ada2041bd97ec92fbaf0c90cd3be4f5c9d
duclux <duclux@gmail.com>
2020-09-14 22:33:44 +0700

    react admin

---
+++ client-admin/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <h1>Jel</h1>
+  )
+}
+
+export default App;
--- server/src/main.ts
+++ server/src/main.ts
@@ -5,13 +5,17 @@ import {
 } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['warn', 'error'],
+    cors: true,
   });
 
   app.setGlobalPrefix('/api');
+  app.use(helmet);
+  // app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
