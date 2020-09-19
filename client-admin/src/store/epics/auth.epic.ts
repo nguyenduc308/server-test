33d27be6daeaab4eeb8481a3acfbabbf91e2b5e6
duc <duc.lux47@gmail.com>
2020-09-19 12:52:54 +0700

    auth login

--- client-admin/src/store/epics/auth.epic.ts
+++ client-admin/src/store/epics/auth.epic.ts
@@ -1,22 +1,39 @@
 import { ActionsObservable, Epic, StateObservable } from 'redux-observable';
 import { Action } from 'redux';
-import { LOGIN } from 'shared/constants/auth-reducer.const';
+import { of } from 'rxjs';
+import { switchMap, filter, catchError } from 'rxjs/operators';
+import { isOfType } from 'typesafe-actions';
+import { map } from 'rxjs/operators';
+
+import {
+  LOGIN,
+  LOGIN_FAILED,
+  LOGIN_SUCCESS,
+} from 'shared/constants/auth-reducer.const';
 import { State } from '../reducers/auth.reducer';
-import { switchMap } from 'rxjs/operators';
 import { ILoginCredentials } from '@type/auth.type';
 import { httpClient } from 'shared/service';
-
-const login = (credentials: ILoginCredentials) => ({
-  type: LOGIN,
-  payload: credentials,
-});
 
 const loginEpic: Epic = (
   action$: ActionsObservable<Action>,
   state$: StateObservable<State>,
 ) =>
-  action$
-    .ofType(login)
-    .pipe(switchMap((data) => httpClient.post('/auth/login', data)));
+  action$.pipe(
+    filter(isOfType(LOGIN)),
+    switchMap((data) =>
+      httpClient.post('/auth/login', data).pipe(
+        catchError((error) =>
+          of({
+            type: LOGIN_FAILED,
+            payload: error,
+          }),
+        ),
+        map((response) => ({
+          type: LOGIN_SUCCESS,
+          payload: response,
+        })),
+      ),
+    ),
+  );
 
 export default loginEpic;
--- client-admin/src/store/reducers/auth.reducer.ts
+++ client-admin/src/store/reducers/auth.reducer.ts
@@ -1,14 +1,24 @@
-import { Action } from 'redux';
-import { LOGIN } from 'shared/constants/auth-reducer.const';
+import {
+  LOGIN_SUCCESS,
+  LOGIN_FAILED,
+} from 'shared/constants/auth-reducer.const';
 
 export type State = {
   user: any;
 };
 
-export default (state = {}, action: Action) => {
+export default (state = {}, action: any) => {
   switch (action.type) {
-    case LOGIN:
-      return state;
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        token: action.payload.token,
+      };
+    case LOGIN_FAILED:
+      return {
+        ...state,
+        token: '',
+      };
     default:
       return state;
   }
